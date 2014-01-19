086963ae5b6acd8e50eabdc3999fdcefbd3f739c
Anna Steinsaphir <anna.steinsaphir@gmail.com>
2014-01-19 17:27:45 +0100

    Basic workout validation.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,8 @@
 class Workout < ActiveRecord::Base
+
+  validates :date, presence: true
+  validates :distance, presence: true, numericality: true
+  validates :duration, presence: true
+  validates :comment, length: { maximum: 500 }
+
 end
