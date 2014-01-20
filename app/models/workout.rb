ec2c339dd2a50cfc6eb0f77004dd6979c4922995
Anna Steinsaphir <anna.steinsaphir@gmail.com>
2014-01-20 19:49:36 +0100

    Average workouts per week.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -8,4 +8,9 @@ class Workout < ActiveRecord::Base
   def self.average_speed
     self.count == 0 ? 0 : self.sum('distance') / (self.sum('duration') / 3600.0)
   end
+
+  def self.average_per_week
+    self.count == 0 ? 0 : self.count.to_f / (Date.parse(self.maximum('date').to_s).cweek - Date.parse(self.minimum('date').to_s).cweek + 1)
+  end
+
 end
--- test/models/workout_test.rb
+++ test/models/workout_test.rb
@@ -19,4 +19,16 @@ class WorkoutTest < ActiveSupport::TestCase
     workout = create :workout, {duration: 30*60, distance: 5}
     assert_equal 10, Workout.average_speed
   end
+
+  def test_average_workouts_per_week_empty
+    assert_equal 0, Workout.average_per_week
+  end
+
+  def test_average_workouts_3weeks
+    create :workout, {date: Time.zone.now}
+    create :workout, {date: 2.week.ago}
+    create :workout, {date: 2.week.ago}
+    assert_equal 1, Workout.average_per_week
+  end
+
 end
