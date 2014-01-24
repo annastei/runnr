672132501ea2a9a7dda29df94bce757397e33dae
Anna Steinsaphir <anna.steinsaphir@gmail.com>
2014-01-24 21:34:29 +0100

    Added tablesorter.

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -17,4 +17,17 @@ $(document).ready(function(){
     update_statistics();
     $("#workout_list").append("<tr><td>" + data.date + "</td><td>" + data.distance + "</td><td>" + data.duration + "</td><td>" + data.comment + "</td><td><a class='btn btn-mini btn-danger' data-confirm='Are you sure?' data-method='delete' href='/workouts/" + data.id + "' rel='nofollow'>Delete</a></td></tr>")
   })
+
+  $(".tablesorter").tablesorter( {
+    headers: { 
+            3: {
+                sorter: false 
+            },
+            4: {
+                sorter: false 
+            }
+        },
+        sortList: [[0,1]]
+  })
+
 });
