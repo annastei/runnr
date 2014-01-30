a0e8afa95a1ce960ada0f43410012f6ddf54455d
Anna Steinsaphir <anna.steinsaphir@gmail.com>
2014-01-30 20:52:52 +0100

    Removed form workouts from routes as well.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Runnr::Application.routes.draw do
   get 'workouts/statistics' => 'workouts#statistics', as: :workout_statistics
 
   resources :workouts
-  resources :form_workouts, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
